288c30a22f80e6e06296d493dafa260713a50c42
JUKKarol <karolprzybylak2@gmail.com>
2024-11-12 20:29:02 +0100

    Init migration

--- api/GarageFlow/Data/AppDbContext.cs
+++ api/GarageFlow/Data/AppDbContext.cs
@@ -6,4 +6,80 @@ namespace GarageFlow.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<AppUser>(options)
 {
+    internal DbSet<Brand> Brands { get; set; }
+    internal DbSet<Car> Cars { get; set; }
+    internal DbSet<Model> Models { get; set; }
+    internal DbSet<Repair> Repair { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Repair>(entity =>
+        {
+            entity.HasKey(r => r.Id);
+
+            entity.Property(r => r.Price).IsRequired();
+
+            entity.HasOne(r => r.Car)
+                .WithMany(c => c.Repairs)
+                .HasForeignKey(r => r.CarId)
+                .OnDelete(DeleteBehavior.NoAction);
+            entity.HasMany(r => r.Users)
+               .WithMany(e => e.Repairs);
+        });
+
+        modelBuilder.Entity<AppUser>(entity =>
+        {
+            entity.HasMany(c => c.Repairs)
+                .WithMany(r => r.Users);
+        });
+
+        modelBuilder.Entity<Car>(entity =>
+        {
+            entity.HasKey(c => c.Id);
+
+            entity.Property(c => c.Engine).IsRequired();
+            entity.Property(c => c.Vin).IsRequired();
+            entity.Property(c => c.yearOfProduction).IsRequired();
+            entity.Property(c => c.ModelId).IsRequired();
+
+            entity.HasOne(c => c.Model)
+                .WithMany(m => m.Cars)
+                .HasForeignKey(c => c.ModelId)
+                .OnDelete(DeleteBehavior.NoAction);
+            entity.HasMany(c => c.Repairs)
+               .WithOne(r => r.Car)
+               .HasForeignKey(r => r.CarId)
+               .OnDelete(DeleteBehavior.NoAction);
+        });
+
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.HasKey(b => b.Id);
+
+            entity.Property(b => b.Name).IsRequired();
+
+            entity.HasMany(b => b.Models)
+                .WithOne(m => m.Brand)
+                .HasForeignKey(m => m.brandId)
+                .OnDelete(DeleteBehavior.NoAction);
+        });
+
+        modelBuilder.Entity<Model>(entity =>
+        {
+            entity.HasKey(m => m.Id);
+
+            entity.Property(m => m.Name).IsRequired();
+
+            entity.HasOne(m => m.Brand)
+                .WithMany(b => b.Models)
+                .HasForeignKey(m => m.brandId)
+                .OnDelete(DeleteBehavior.NoAction);
+            entity.HasMany(m => m.Cars)
+                .WithOne(c => c.Model)
+                .HasForeignKey(c => c.ModelId)
+                .OnDelete(DeleteBehavior.NoAction);
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
--- api/GarageFlow/Entities/AppUser.cs
+++ api/GarageFlow/Entities/AppUser.cs
@@ -4,4 +4,5 @@ namespace GarageFlow.Entities;
 
 public class AppUser : IdentityUser
 {
+    public List<Repair> Repairs { get; set; }
 }
