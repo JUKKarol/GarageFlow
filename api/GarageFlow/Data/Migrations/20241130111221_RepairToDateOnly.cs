7840cd24b33e9f0048258e5667916fc9ef8c0882
JUKKarol <karolprzybylak2@gmail.com>
2024-11-30 12:13:39 +0100

    Change repair props to DateOnly

---
+++ api/GarageFlow/Data/Migrations/20241130111221_RepairToDateOnly.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GarageFlow.Migrations
+{
+    /// <inheritdoc />
+    public partial class RepairToDateOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "StartedEstimateAt",
+                table: "Repairs",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "StartedAt",
+                table: "Repairs",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "FinishedAt",
+                table: "Repairs",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "FinisheEstimatedAt",
+                table: "Repairs",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartedEstimateAt",
+                table: "Repairs",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartedAt",
+                table: "Repairs",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FinishedAt",
+                table: "Repairs",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FinisheEstimatedAt",
+                table: "Repairs",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
--- api/GarageFlow/Entities/Repair.cs
+++ api/GarageFlow/Entities/Repair.cs
@@ -7,10 +7,10 @@ public class Repair
     public Guid Id { get; set; } = Guid.NewGuid();
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime StartedAt { get; set; }
-    public DateTime StartedEstimateAt { get; set; }
-    public DateTime FinishedAt { get; set; }
-    public DateTime FinisheEstimatedAt { get; set; }
+    public DateOnly StartedAt { get; set; }
+    public DateOnly StartedEstimateAt { get; set; }
+    public DateOnly FinishedAt { get; set; }
+    public DateOnly FinisheEstimatedAt { get; set; }
     public int Price { get; set; }
     public int PriceEstimate { get; set; }
     public string Description { get; set; }
