7b115822878a6288095bd27cc82bb2c715930fe6
JUKKarol <karolprzybylak2@gmail.com>
2025-01-10 00:01:39 +0100

    Add command validator for Repair

---
+++ api/GarageFlow/CQRS/Repair/Commands/CreateRepair/CreateRepairCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace GarageFlow.CQRS.Repair.Commands.CreateRepair;
+
+public class CreateRepairCommandValidator : AbstractValidator<CreateRepairCommand>
+{
+    public CreateRepairCommandValidator()
+    {
+        RuleFor(c => c.PlannedFinishAt)
+            .NotEmpty();
+        RuleFor(c => c.PlannedStartAt)
+            .NotEmpty();
+        RuleFor(c => c.Description)
+            .Length(1, 1000);
+        RuleFor(c => c.customerName)
+            .NotEmpty()
+            .Length(1, 50);
+        RuleFor(c => c.customerPhoneNumber)
+            .Length(9, 15);
+        RuleFor(c => c.customerEmail)
+            .EmailAddress()
+            .Length(1, 50);
+    }
+}
---
+++ api/GarageFlow/CQRS/Repair/Commands/UpdateRepair/UpdateRepairCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using GarageFlow.Enums;
+
+namespace GarageFlow.CQRS.Repair.Commands.UpdateRepair;
+
+public class UpdateRepairCommandValidator : AbstractValidator<UpdateRepairCommand>
+{
+    public UpdateRepairCommandValidator()
+    {
+        RuleFor(c => c.StartedAt);
+        RuleFor(c => c.FinishedAt);
+        RuleFor(c => c.Price)
+            .InclusiveBetween(1, 1000000000);
+        RuleFor(c => c.Description)
+            .Length(1, 1000);
+        RuleFor(c => c.CustomerName)
+            .NotEmpty()
+            .Length(1, 50);
+        RuleFor(c => c.CustomerPhoneNumber)
+            .Length(9, 15);
+        RuleFor(c => c.CustomerEmail)
+            .EmailAddress()
+            .Length(1, 50);
+        RuleFor(c => c.Status)
+            .IsInEnum();
+        RuleFor(c => c.CarId);
+        RuleFor(c => c.Users);
+    }
+}
