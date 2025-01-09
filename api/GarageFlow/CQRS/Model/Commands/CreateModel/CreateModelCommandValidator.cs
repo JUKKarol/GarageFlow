d84b1e721707204260bc12be357992292f0b759b
JUKKarol <karolprzybylak2@gmail.com>
2025-01-09 13:45:48 +0100

    Add command validator for model

---
+++ api/GarageFlow/CQRS/Model/Commands/CreateModel/CreateModelCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace GarageFlow.CQRS.Model.Commands.CreateModel;
+
+public class CreateModelCommandValidator : AbstractValidator<CreateModelCommand>
+{
+    public CreateModelCommandValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty()
+            .Length(1, 50);
+        RuleFor(c => c.BrandId)
+            .NotEmpty();
+    }
+}
---
+++ api/GarageFlow/CQRS/Model/Commands/UpdateModel/UpdateModelCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace GarageFlow.CQRS.Model.Commands.UpdateModel;
+
+public class UpdateModelCommandValidator : AbstractValidator<UpdateModelCommand>
+{
+    public UpdateModelCommandValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty()
+            .Length(1, 50);
+        RuleFor(c => c.Id)
+            .NotEmpty();
+    }
+}
