96ea061caedd366c6ee0e7a4e775da9a4ec9f671
JUKKarol <karolprzybylak2@gmail.com>
2025-01-13 15:32:11 +0100

    Fix correct spelling of Employee

--- api/GarageFlow/Constants/UserRoles.cs
+++ api/GarageFlow/Constants/UserRoles.cs
@@ -3,6 +3,6 @@
 public static class UserRoles
 {
     public const string Client = "Client";
-    public const string Employee = "Emloyee";
+    public const string Employee = "Employee";
     public const string Admin = "Admin";
 }
