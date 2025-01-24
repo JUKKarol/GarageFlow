1508f34827e9a6db57b241014d0cd2054649d62c
JUKKarol <karolprzybylak2@gmail.com>
2025-01-24 17:41:32 +0100

    Fix configuration for sieve repair

--- api/GarageFlow/Utilities/Sieve/SieveConfiguration/SieveConfigurationForRepair.cs
+++ api/GarageFlow/Utilities/Sieve/SieveConfiguration/SieveConfigurationForRepair.cs
@@ -7,14 +7,52 @@ public class SieveConfigurationForRepair : ISieveConfiguration
 {
     public void Configure(SievePropertyMapper mapper)
     {
+        mapper.Property<Repair>(r => r.Id)
+           .CanFilter();
+
         mapper.Property<Repair>(r => r.CreatedAt)
+            .CanFilter()
             .CanSort();
 
         mapper.Property<Repair>(r => r.UpdatedAt)
+            .CanFilter()
+            .CanSort();
+
+        mapper.Property<Repair>(r => r.StartedAt)
+            .CanFilter()
+            .CanSort();
+
+        mapper.Property<Repair>(r => r.PlannedStartAt)
+            .CanFilter()
+            .CanSort();
+
+        mapper.Property<Repair>(r => r.FinishedAt)
+            .CanFilter()
+            .CanSort();
+
+        mapper.Property<Repair>(r => r.PlannedFinishAt)
+            .CanFilter()
             .CanSort();
 
         mapper.Property<Repair>(r => r.Description)
-            .CanFilter();
+            .CanFilter()
+            .CanSort();
+
+        mapper.Property<Repair>(r => r.CustomerName)
+            .CanFilter()
+            .CanSort();
+
+        mapper.Property<Repair>(r => r.CustomerPhoneNumber)
+            .CanFilter()
+            .CanSort();
+
+        mapper.Property<Repair>(r => r.CustomerEmail)
+            .CanFilter()
+            .CanSort();
+
+        mapper.Property<Repair>(r => r.CarId)
+            .CanFilter()
+            .CanSort();
 
         mapper.Property<Repair>(r => r.Status)
             .CanFilter()
