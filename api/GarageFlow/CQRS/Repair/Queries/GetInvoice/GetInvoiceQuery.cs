6cec4e4f6a4385739c7764e2ecb9cf2ed86c3f6c
JUKKarol <karolprzybylak2@gmail.com>
2025-01-30 11:03:27 +0100

    Generate pdf while triggering invoice endpoint

--- api/GarageFlow/CQRS/Repair/Queries/GetInvoice/GetInvoiceQuery.cs
+++ api/GarageFlow/CQRS/Repair/Queries/GetInvoice/GetInvoiceQuery.cs
@@ -1,8 +1,9 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GarageFlow.CQRS.Repair.Queries.GetInvoice;
 
-public class GetInvoiceQuery : IRequest<InvoiceResponse>
+public class GetInvoiceQuery : IRequest<IActionResult>
 {
     public Guid RepairId { get; set; }
     public string CustomerAddress { get; set; }
--- api/GarageFlow/CQRS/Repair/Queries/GetInvoice/GetInvoiceQueryHandler.cs
+++ api/GarageFlow/CQRS/Repair/Queries/GetInvoice/GetInvoiceQueryHandler.cs
@@ -5,14 +5,18 @@ using GarageFlow.Middlewares.Exceptions;
 using GarageFlow.Repositories.RepairDetailRepository;
 using GarageFlow.Repositories.RepairRepository;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using PdfSharp.Pdf;
 
 namespace GarageFlow.CQRS.Repair.Queries.GetInvoice;
 
 public class GetInvoiceQueryHandler(IMapper mapper,
     IRepairRepository repairRepository,
-    IRepairDetailRepository repairDetailRepository) : IRequestHandler<GetInvoiceQuery, InvoiceResponse>
+    IRepairDetailRepository repairDetailRepository) : IRequestHandler<GetInvoiceQuery, IActionResult>
 {
-    public async Task<InvoiceResponse> Handle(GetInvoiceQuery request, CancellationToken cancellationToken)
+    public async Task<IActionResult> Handle(GetInvoiceQuery request, CancellationToken cancellationToken)
     {
         var repair = await repairRepository.GetRepairById(request.RepairId, cancellationToken);
 
@@ -34,6 +38,41 @@ public class GetInvoiceQueryHandler(IMapper mapper,
         invoice.RepairDetails = mapper.Map<List<RepairDetailResponse>>(repairDetails);
         invoice.Price = invoice.RepairDetails.Sum(rd => rd.Price);
 
-        return invoice;
+        var document = GeneratePdf();
+        MemoryStream stream = new();
+        document.Save(stream);
+
+        byte[] bytes = stream.ToArray();
+        stream.Close();
+
+        return new FileContentResult(bytes, "application/pdf")
+        {
+            FileDownloadName = "invoice.pdf"
+        };
+
+        //return invoice;
+    }
+
+    public PdfDocument GeneratePdf()
+    {
+        var document = new Document();
+
+        BuildDocument(document);
+
+        var pdfRenderer = new PdfDocumentRenderer();
+
+        pdfRenderer.Document = document;
+
+        pdfRenderer.RenderDocument();
+
+        return pdfRenderer.PdfDocument;
+    }
+
+    private void BuildDocument(Document document)
+    {
+        Section section = document.AddSection();
+
+        var paragraph = section.AddParagraph();
+        paragraph.AddText("Invoice");
     }
 }
