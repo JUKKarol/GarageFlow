c33b9508f383541bfa1b19ebded28ea819649d36
q0ney <98429622+q0ney@users.noreply.github.com>
2025-02-13 17:38:15 +0100

    auth user can only access "/admin" path

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -2,31 +2,37 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+  const path = req.nextUrl.pathname;
 
-  const token = req.cookies.get("token")?.value; 
-
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
+  if (token && (path === "/" || path === "/login")) {
+    return NextResponse.redirect(new URL("/admin", req.url));
   }
 
-  const userRolesString = req.cookies.get("userRoles")?.value || '[]';
-  
-  try {
-    const roles = JSON.parse(userRolesString);
-
-    const requiredRoles = ["Admin", "Employee"];
-    const hasAccess = roles.some((role: string) => requiredRoles.includes(role));
-
-    if (!hasAccess) {
-      return NextResponse.redirect(new URL("/", req.url));
+  // Handle /admin routes
+  if (path.startsWith("/admin")) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", req.url));
     }
-  } catch (error) {
-    return NextResponse.redirect(new URL("/login", req.url));
+
+    const userRolesString = req.cookies.get("userRoles")?.value || '[]';
+    
+    try {
+      const roles = JSON.parse(userRolesString);
+      const requiredRoles = ["Admin", "Employee"];
+      const hasAccess = roles.some((role: string) => requiredRoles.includes(role));
+
+      if (!hasAccess) {
+        return NextResponse.redirect(new URL("/", req.url));
+      }
+    } catch (error) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/admin/:path*", 
+  matcher: ["/", "/login", "/admin/:path*"],
 };
